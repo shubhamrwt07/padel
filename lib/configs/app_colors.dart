efb0c47a9980ca948cdcd76a3f4a22ce7d9b9b34
suraj bhandari <surajbhandari@192.168.1.19>
2025-06-23 10:12:04 +0530

    just pull

--- lib/configs/app_colors.dart
+++ lib/configs/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  ///
+  ///////
   static const Color primaryColor = Color(0xFF1F41BB);
   static const Color secondaryColor = Color(0xFF3DBE64);
   static const Color textColor = Color(0xFF626262);
